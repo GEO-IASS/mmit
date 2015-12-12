df7cf0f77717fa8b1eee7c49097a4bed14e2e0f0
adalca <adalca@mit.edu>
2015-12-12 15:09:09 -0500

    expand padNii with a pad type

--- src/padNii.m
+++ src/padNii.m
@@ -1,4 +1,4 @@
-function padNii(niiFileName, paddedNiiFileName, maskNiiFileName, padAmountMM)
+function padNii(niiFileName, paddedNiiFileName, maskNiiFileName, padAmountMM, padType)
 % PADNII pad a nifti and save the nifti and the relevant mask.
 %   padNii(niiFileName, paddedNiiFileName, maskNiiFileName) pad nifti file
 %   niiFileName and save the resulting nifti in paddedNiiFileName, and the relevant mask in 
@@ -6,6 +6,9 @@ function padNii(niiFileName, paddedNiiFileName, maskNiiFileName, padAmountMM)
 %
 %   padNii(niiFileName, paddedNiiFileName, maskNiiFileName, padAmountMM) allows one to
 %   specify the padding amount in mm via padAmountMM.
+%
+%   padNii(niiFileName, paddedNiiFileName, maskNiiFileName, padAmountMM, padType)
+%       padType can be 'both', 'pre', 'post'
 %
 %   Example arguments:
 %   niiFileName = '10529_t1.nii.gz';
@@ -22,6 +25,11 @@ elseif ischar(padAmountMM)
 	padAmountMM = str2num(padAmountMM); 
 end
 
+if ~exist('padType', 'var')
+    padType = 'both'; 
+end
+
+
 % load the nifti
 nii = loadNii(niiFileName, tempdir, true);
 
@@ -30,11 +38,28 @@ pixdim = nii.hdr.dime.pixdim(2:4);
 padAmount = ceil(padAmountMM ./ pixdim);
 dims = nii.hdr.dime.dim(2:4);
 assert(all(size(padAmount) == size(dims)));
-newDims = dims + padAmount * 2;
 
-% extract the subvolume and mask
-vol = setSubvolume(zeros(newDims), nii.img);
-volMask = setSubvolume(zeros(newDims), ones(dims));
+switch padType
+    case 'both'
+        newDims = dims + padAmount * 2;
+
+        % extract the subvolume and mask
+        vol = setSubvolume(zeros(newDims), nii.img);
+        volMask = setSubvolume(zeros(newDims), ones(dims));
+    case 'pre'
+        newDims = dims + padAmount;
+        vol = setSubvolume(zeros(newDims), nii.img, padAmount + 1, newDims);
+        volMask = setSubvolume(zeros(newDims), ones(dims), padAmount + 1, newDims);
+
+    case 'post'
+        newDims = dims + padAmount;
+        o = ones(1, numel(newDims));
+        vol = setSubvolume(zeros(newDims), nii.img, o, dims);
+        volMask = setSubvolume(zeros(newDims), ones(dims), o, dims);
+        
+    otherwise
+        error('padNii: Unknown padType');
+end
 
 % create the niftis
 newNii = makeNiiLike(vol, nii);
