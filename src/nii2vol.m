c75cc91f20078ffed041424e68c69640c989b4fd
adalca <adalca@mit.edu>
2016-06-10 17:54:45 -0400

    better error treatment in nii2vol

--- src/nii2vol.m
+++ src/nii2vol.m
@@ -2,6 +2,8 @@ function vol = nii2vol(nii)
 
     if ischar(nii) && sys.isfile(nii)
         nii = loadNii(nii);
+    elseif ischar(nii)
+        error('%s is not a nifti file', nii);
     else
         assert(isstruct(nii));
     end
